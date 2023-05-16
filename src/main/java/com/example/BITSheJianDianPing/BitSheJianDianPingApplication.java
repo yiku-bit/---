6b43caf00c51e822ead878ac1a9aab80f57fedac
Sun Ruixiang <2928026220@qq.com>
2023-05-16 15:06:37 +0800

    Initial commit

---
+++ src/main/java/com/example/BITSheJianDianPing/BitSheJianDianPingApplication.java
@@ -0,0 +1,13 @@
+package com.example.BITSheJianDianPing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BitSheJianDianPingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BitSheJianDianPingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BITSheJianDianPing/BitSheJianDianPingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BITSheJianDianPing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BitSheJianDianPingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
