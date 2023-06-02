172e4c9422ff0d3eb8182dedef495dea154ab2e8
Fu Guangming <another_answer@163.com>
2023-06-02 22:16:25 +0800

    vertification code

--- src/main/java/com/example/BITSheJianDianPing/bean/VertifyCode.java
+++ src/main/java/com/example/BITSheJianDianPing/bean/VertifyCode.java
@@ -4,13 +4,7 @@ package com.example.BITSheJianDianPing.bean;
 
 public class VertifyCode {
     private String code;
-
-    public VertifyCode() {
-    }
-
-    public VertifyCode(String code) {
-        this.code = code;
-    }
+    private String phoNum;
 
     public String getCode() {
         return code;
@@ -20,10 +14,27 @@ public class VertifyCode {
         this.code = code;
     }
 
+    public String getPhoNum() {
+        return phoNum;
+    }
+
+    public void setPhoNum(String phoNum) {
+        this.phoNum = phoNum;
+    }
+
+    public VertifyCode() {
+    }
+
+    public VertifyCode(String code, String phoNum) {
+        this.code = code;
+        this.phoNum = phoNum;
+    }
+
     @Override
     public String toString() {
         return "VertifyCode{" +
                 "code='" + code + '\'' +
+                ", phoNum='" + phoNum + '\'' +
                 '}';
     }
 }
